e5836210a3ba0bd54b4a13c61bd553db1ec3d993
kittipithinkit <kittipithm@gmail.com>
2025-12-26 23:47:01 +0700

    lap10(3)complete

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,35 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ifstream source;
+    source.open("score.txt");
+    
+    double sum = 0;
+    double sum_sq = 0;
+    double count = 0;
+    double x;
+
+    while (source >> x) {
+        sum += x;
+        sum_sq += x * x;
+        count++;
+    }
+
+    if (count > 0) {
+        double mean = sum / count;
+        double std_deviation = sqrt((sum_sq / count) - (mean * mean));
+
+        cout << "Number of data = " << count << endl;
+        cout << setprecision(3);
+        cout << "Mean = " << mean << endl;
+        cout << "Standard deviation = " << std_deviation << endl;
+    }
+
+    source.close();
+    return 0;
+}
